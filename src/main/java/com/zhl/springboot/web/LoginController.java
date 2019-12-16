544739194bbee376c97f7fc13592c4458ef8b945
Zhuanle <1847496393@qq.com>
2019-12-16 15:43:40 +0800

    springboot1

--- src/main/java/com/zhl/springboot/web/LoginController.java
+++ src/main/java/com/zhl/springboot/web/LoginController.java
@@ -4,10 +4,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
-    @RequestMapping("/hello")
-    public String hello(){
+public class LoginController {
 
-        return "Hello Spring Boot git!";
+    @RequestMapping("/login")
+    public String login(){
+        return "zhl";
     }
 }
