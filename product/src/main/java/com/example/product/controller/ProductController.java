fac4932933b8ccc2b17dfb4e90d7c54af3d3c795
Zhuanle <1847496393@qq.com>
2019-12-25 10:35:08 +0800

    spring boot1.1

--- product/src/main/java/com/example/product/controller/ProductController.java
+++ product/src/main/java/com/example/product/controller/ProductController.java
@@ -19,8 +19,6 @@ import java.util.List;
   }
 
   @RequestMapping("/listProduct") public List<Product> list() {
-    List<Product> list = new ArrayList<>();
-    list.add()
     return productService.list();
   }
 }
